293e1cefbfa1b96031a4920f4813248deec754d1
Aleksey <oldf03@gmail.com>
2022-05-28 12:04:22 +0300

    Initial Commit

---
+++ Find couple/HardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HardViewController.swift
+//  Find couple
+//
+//  Created by Buba on 28.05.2022.
+//
+
+import UIKit
+
+class HardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Find couple/MenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuViewController.swift
+//  Find couple
+//
+//  Created by Buba on 28.05.2022.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+}
